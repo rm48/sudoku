cc6d0e13b5e70b1f7954dcf846a587ee05184655
reinaldo <reinaldo58972@gmail.com>
2025-04-15 18:03:05 -0300

    main, consumindo o projeto

--- src/model/GameStatusEnum.java
+++ src/model/GameStatusEnum.java
@@ -1,7 +1,17 @@
 package model;
 
 public enum GameStatusEnum {
-	NON_STARTED,
-	INCOMPLETE,
-	COMPLETE
+	NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+	
+	private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+	public Object getLabel() {
+		return label;
+	}
 }
